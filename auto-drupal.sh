5880291dee005214e2317bd60fc1c3e5d6b11a8d
liao-chianan <lcn@syps.tp.edu.tw>
2016-09-26 20:01:13 +0800

    Update auto-drupal.sh

--- auto-drupal.sh
+++ auto-drupal.sh
@@ -62,6 +62,21 @@ echo " ========================================================"
 
 docker run --restart=always  --name mysql -e MYSQL_ROOT_PASSWORD=$mysql_pw -e MYSQL_DATABASE=drupal  -d mysql/mysql-server
 
+mysql_ip=$(docker inspect mysql|grep \"IPAddress\" | awk 'NR==1{gsub ( "\"","" );gsub ( ",","" ); print $2 }')
+
+if [ -f /etc/redhat-release ]; then
+
+	while ! printf "." | nc  $mysql_ip 3306 &> /dev/null; do sleep 11; done    
+
+else
+   
+                until nc -z -v -w30 $mysql_ip 3306 &> /dev/null
+                do
+                         printf "."
+                        sleep 1
+                done
+fi
+
 printf "\nWaiting Mysql Container 3306 socket."
 
                 mysql_ip=$(docker inspect mysql|grep \"IPAddress\" | awk 'NR==1{gsub ( "\"","" );gsub ( ",","" ); print $2 }')
