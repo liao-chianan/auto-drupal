2cc77df05db2302bb3cfb601ac5b313dc96caef8
liao-chianan <lcn@syps.tp.edu.tw>
2016-09-26 18:21:45 +0800

    Update d4s-auto-build.sh

--- d4s-auto-build.sh
+++ d4s-auto-build.sh
@@ -1,41 +1,63 @@
-mkdir ~/drupal4school
-cd ~/drupal4school
-git clone https://github.com/liao-chianan/drupal4school
-cd ~/drupal4school
+d4srepo="fosstp/drupal4school"
+htmlroot="/var/www/html/"
+
+cd /root/"$d4srepo"
 
 check_remote=$(git pull origin)
 check_time=$(date +"%Y%m%d_%T")
-if [[ "$check_remote" == "Already up-to-date." ]]; then
+
+sed '28d' -i "$htmlroot"index.html
+sed "28i  最近一次檢查時間LastCheck at <a href=log/checkanddeploy.log>$check_time</a></br>" -i "$htmlroot"index.html
+
+if [[ "$(git pull origin)" == *"Already up-to-date."* ]]; then
 	echo "$check_time   Already up-to-date,Do not need build!"
+	echo "$check_time   Already up-to-date,Do not need build!" > "$htmlroot"log/checkanddeploy.log
+	sed '29d' -i "$htmlroot"index.html
+	sed "29i 測試網站 Running  <a href=http://163.21.204.168 target_blank>$(git reflog | awk 'NR==1{print $1}')</a>" -i "$htmlroot"index.html
 else
-	git_pull_reflog=$(git reflog --date=iso  master | awk 'NR==1{gsub ( "master@{","master_" ) ; print $1"_"$2"_"$3; }')
+	git_pull_reflog=$(git reflog --pretty='%cd %h' origin | awk 'NR==1{gsub(":",""); print "HEAD_"$7"_"$2"_"$3"_"$4"_"$5; }')
 	echo $git_pull_reflog
+	sed '29d' -i "$htmlroot"index.html
+	sed "29i Start Building $git_pull_reflog....plz wait" -i "$htmlroot"index.html
+	docker_build_tag=$(git reflog --pretty='%cd %h' origin | awk 'NR==1{ print $7; }')
 
-	docker_build_tag=$(git reflog --date=iso  master | awk 'NR==1{gsub ( "master@{","master_" ) ; print $1 }')
-	docker build -t "d4s-$docker_build_tag" ~/drupal4school > ~/"$git_pull_reflog.log"
+
+	docker build -t "d4s-$docker_build_tag" /root/"$d4srepo" > "$htmlroot"log/"$git_pull_reflog.log"
 
 
-	check_build=$(tail -n 1 ~/"$git_pull_reflog.log")
-	
+	check_build=$(tail -n 1 "$htmlroot"log/"$git_pull_reflog.log")
+	echo $check_build	
 
 	if [[ "$check_build" == *"Successfully built"* ]]; then
+		sed '29d' -i "$htmlroot"index.html
+	        sed "29i $git_pull_reflog  Building Succesfull , now Deploy Drupal4School...wait again plz"  -i "$htmlroot"index.html
+
+		
+		imageid=$(tail -n 1 "$htmlroot"log/"$git_pull_reflog.log" | awk '{print $3}')
 		echo "successfull....deployment d4s website"
-		imageid=$(tail -n 1 ~/"$git_pull_reflog.log" | awk '{print $3}')
+		echo "</br>Git Repo "$git_pull_reflog" build  Successfull build time "$(date +"%Y%m%d_%T")" imageid="$imageid" <a href=log/$git_pull_reflog.log target=_blank>Buildlog</a>"  >> "$htmlroot"index.html
 		docker stop $(docker ps -a -q)
 		docker rm -f $(docker ps -a -q)
-		docker rmi -f $(docker images -q| awk '$0 != "'"$imageid"'" {print}') 
+		docker pull mysql/mysql-server
+		docker pull drupal:7
 	
 		docker run --restart=always  --name mysql -e MYSQL_ROOT_PASSWORD=1234 -e MYSQL_DATABASE=drupal  -d mysql/mysql-server
-		printf "\nWaiting Mysql Container Loading."
-		until [ $(docker inspect mysql|grep \"Pid\" | awk '{print  $2}'|sed -r "s/[,]+//g") -ne "0" ]; do
-		        printf "."
-		        sleep 1
-		done
-		printf "Complete\n"
+		printf "\nWaiting Mysql Container 3306 socket."
+
+	        mysql_ip=$(docker inspect mysql|grep \"IPAddress\" | awk 'NR==1{gsub ( "\"","" );gsub ( "\,","" ); print $2 }')
+
+                until nc -z -v -w30 $mysql_ip 3306 &> /dev/null
+                do
+                         printf "."
+                        sleep 1
+                done
+
+                printf "Complete\n"
+
 
 		
 		docker run --restart=always  --name drupal --link mysql:db -p 80:80 -p 443:443 -d "d4s-$docker_build_tag"
-		docker exec  drupal drush -y  site-install standard --clean-url=0 --site-name=$docker_build_tag --account-pass=1234 --db-url=mysql://root:1234@db/drupal
+		docker exec  drupal drush -y  site-install standard --clean-url=0 --site-name="$docker_build_tag" --account-pass=1234 --db-url=mysql://root:1234@db/drupal
 		docker exec  drupal cp /etc/php5/cli/php.ini  /usr/local/etc/php/
 		docker exec  drupal sed -i 's/memory_limit = -1/memory_limit = 256M/g' /usr/local/etc/php/php.ini
 		docker exec  drupal drush dl drush_language -y
@@ -51,8 +73,13 @@ else
 		docker exec  drupal chown -R www-data:www-data /var/www/html/sites/default
 		docker exec  drupal chmod -R 755 /var/www/html/sites/default/files
 		docker restart drupal
+		
+		sed '29d' -i "$htmlroot"index.html
+                sed "29i Deploy complete , now Running <a href=http://163.21.204.168 target=_blank>$git_pull_reflog</a>"  -i "$htmlroot"index.html
 	fi
-	
+        if [[ "$check_build" != *"Successfully built"* ]]; then
+
+	echo "</br>Git Head "$git_pull_reflog" build failed build time "$(date +"%Y%m%d_%T")" imageid=no-image <a href=log/$git_pull_reflog.log target=_blank>Buildlog</a>"  >> "$htmlroot"index.html
+	fi
 
 fi
-
